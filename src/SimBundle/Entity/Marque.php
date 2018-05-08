b966c198ba14fb3d3cf955d87abf186faa210714
trabelsi radhwen <trabelsi.radhouen@gmail.com>
2018-05-08 01:28:50 +0100

    add repository class

--- src/SimBundle/Entity/Marque.php
+++ src/SimBundle/Entity/Marque.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="SimBundle\Repository\MarqueRepository")
  * @ORM\Table(name="marque")
  */
 class Marque
