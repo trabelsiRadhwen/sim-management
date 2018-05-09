784554b43908332b8ee1b05ca309259854b114b0
trabelsi radhwen <trabelsi.radhouen@gmail.com>
2018-05-09 19:31:06 +0100

    database migration

---
+++ app/DoctrineMigrations/Version20180508233012.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180508233012 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sim CHANGE numero_serie numero_serie INT NOT NULL, CHANGE numero_appel numero_appel INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sim CHANGE numero_serie numero_serie VARCHAR(10) NOT NULL COLLATE utf8_unicode_ci, CHANGE numero_appel numero_appel VARCHAR(8) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
